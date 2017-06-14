649aba0eb6185a54e1cbb2946b3fb437a33d10c1
tanc-ubuntu <gogotanc@163.com>
2017-06-14 18:53:50 +0800

    update

--- src/main/java/org/offer/case41/ContinuesSequenceWithSum.java
+++ src/main/java/org/offer/case41/ContinuesSequenceWithSum.java
@@ -4,7 +4,7 @@ package org.offer.case41;
  * 面试题 41: 和为 S 的连续正数序列
  * Created by tanc on 17-6-14.
  */
-public class ContinusSequenceWithSum {
+public class ContinuesSequenceWithSum {
 
     public static void methodOne(int[] data, int s) {
 
