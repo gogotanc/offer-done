d9e81c61216e26db98714c34119cb67024e848ab
tanc-ubuntu <gogotanc@163.com>
2017-06-19 18:21:02 +0800

    finished 38

--- src/main/java/org/offer/case38/GetNumberOfK.java
+++ src/main/java/org/offer/case38/GetNumberOfK.java
@@ -6,7 +6,60 @@ package org.offer.case38;
  */
 public class GetNumberOfK {
 
+    /**
+     * 方法一：直接遍历
+     */
     public static int methodOne(int[] data, int k) {
+        if (null == data || data.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i : data) {
+            if (k == i) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 方法二：二分查找
+     */
+    public static int methodTwo(int[] data, int k) {
+        if (null == data || data.length == 0) {
+            return 0;
+        }
+
+        int leftIndex = findLeft(data, 0, data.length - 1, k);
+        if (leftIndex == -1) {
+            // data 数组中没有 k
+            return 0;
+        }
+        int rightIndex = findRight(data, 0, data.length - 1, k);
+        return rightIndex - leftIndex + 1;
+    }
+
+    // 找开始的位置
+    private static int findLeft(int[] data, int start, int end, int k) {
+        if (start == end) {
+            if (data[start] == k) {
+                return start;
+            } else {
+                return -1;
+            }
+        }
+        int middle = (end - start + 1) / 2;
+        if (data[middle] > k) {
+            return findLeft(data, start, middle, k);
+        } else if (data[middle] == k) {
+            return 0;
+        } else {
+            return findLeft(data, middle, end, k);
+        }
+    }
+
+    // 找结束的位置
+    private static int findRight(int[] data, int start, int end, int k) {
         return 0;
     }
 }
